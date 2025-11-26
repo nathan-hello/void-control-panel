0ae9cb1eff8ee4a029e0ceb57264311cfd06a200
nate <natey3k@gmail.com>
2025-11-25 19:14:55 -0500

    format

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
-#include <gtkmm-4.0/gtkmm/application.h>
-#include <gtkmm-4.0/gtkmm/button.h>
-#include <gtkmm-4.0/gtkmm/window.h>
-
-
+#include <gtkmm/application.h>
+#include <gtkmm/button.h>
+#include <gtkmm/window.h>
 
 class HelloWorld : public Gtk::Window {
-Gtk::Button m_button;
+  Gtk::Button m_button;
+
 
 public:
   HelloWorld() {
     m_button = Gtk::Button("Asdf");
     m_button.set_margin(10);
-    m_button.signal_clicked().connect(sigc::mem_fun(*this, &HelloWorld::on_button_click));
+    m_button.signal_clicked().connect(
+        sigc::mem_fun(*this, &HelloWorld::on_button_click));
     set_child(m_button);
   };
+  void Asdf() {}
 
-protected:
-  void on_button_click(){
-    m_button.set_label("Fdsa");
-  };
+private:
+  void on_button_click() { m_button.set_label("Fdsa"); };
 };
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   auto app = Gtk::Application::create("mywindow");
   return app->make_window_and_run<HelloWorld>(argc, argv);
 }

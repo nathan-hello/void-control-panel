ce7624c444b97313aca87379e41f1145ffe2cec2
nate <natey3k@gmail.com>
2025-11-28 21:48:32 -0500

    memes

--- main.cpp
+++ main.cpp
@@ -2,25 +2,31 @@
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
 
-class HelloWorld : public Gtk::Window {
+class MainWindow : public Gtk::Window {
   Gtk::Button m_button;
 
 
 public:
-  HelloWorld() {
+  MainWindow() {
     m_button = Gtk::Button("Asdf");
     m_button.set_margin(10);
     m_button.signal_clicked().connect(
-        sigc::mem_fun(*this, &HelloWorld::on_button_click));
+        sigc::mem_fun(*this, &MainWindow::on_button_click));
     set_child(m_button);
   };
-  void Asdf() {}
 
 private:
-  void on_button_click() { m_button.set_label("Fdsa"); };
+  void on_button_click() {
+    std::string current_label = m_button.get_label();
+    if (current_label == "Asdf") {
+    m_button.set_label("Fdsa"); 
+    } else {
+      m_button.set_label("Asdf");
+    }
+  };
 };
 
 int main(int argc, char *argv[]) {
   auto app = Gtk::Application::create("mywindow");
-  return app->make_window_and_run<HelloWorld>(argc, argv);
+  return app->make_window_and_run<MainWindow>(argc, argv);
 }
